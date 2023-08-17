2c7c791c852cfbe1c019cdb747e8fdf0cfcb8dfc
kuttu101996 <kuttulr1996@gmail.com>
2023-08-18 04:16:21 +0530

    change localhost with deployed frontend url

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ const server = app.listen(process.env.port, async () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://comm-u-cate.netlify.app/",
   },
 });
 
